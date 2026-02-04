1ad09dca714d08a13d3abdc7174acf40ffc830c4
tpq-classes <admin@tpq.io>
2026-02-04 11:11:09 +0100

    Updated to solve TF/Keras eager mode problem

--- dqlagent.py
+++ dqlagent.py
@@ -12,16 +12,16 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from collections import deque
-from keras.layers import Dense, Flatten
-from keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
 
 warnings.simplefilter('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-from tensorflow.python.framework.ops import disable_eager_execution
-disable_eager_execution()
+#from tensorflow.python.framework.ops import disable_eager_execution
+#disable_eager_execution()
 
-opt = keras.optimizers.legacy.Adam
+opt = keras.optimizers.Adam
 
 
 class DQLAgent:
@@ -38,7 +38,7 @@ class DQLAgent:
         self.env = env
         self.episodes = 0
         self._create_model(hu, lr)
-        
+
     def _create_model(self, hu, lr):
         self.model = Sequential()
         self.model.add(Dense(hu, activation='relu',
@@ -46,28 +46,55 @@ class DQLAgent:
         self.model.add(Dense(hu, activation='relu'))
         self.model.add(Dense(2, activation='linear'))
         self.model.compile(loss='mse', optimizer=opt(learning_rate=lr))
-        
+
     def _reshape(self, state):
         state = state.flatten()
         return np.reshape(state, [1, len(state)])
-            
+
     def act(self, state):
         if random.random() < self.epsilon:
             return self.env.action_space.sample()
-        return np.argmax(self.model.predict(state)[0])
-        
+
+        #return np.argmax(self.model.predict(state)[0])
+
+        q = self.model(tf.convert_to_tensor(state, dtype=tf.float32), training=False)
+        return int(tf.argmax(q[0]).numpy())
+
     def replay(self):
         batch = random.sample(self.memory, self.batch_size)
-        for state, action, next_state, reward, done in batch:
-            if not done:
-                reward += self.gamma * np.amax(
-                    self.model.predict(next_state)[0])
-                target = self.model.predict(state)
-                target[0, action] = reward
-                self.model.fit(state, target, epochs=1, verbose=False)
+
+
+        #for state, action, next_state, reward, done in batch:
+        #    if not done:
+        #        reward += self.gamma * np.amax(
+        #            self.model.predict(next_state)[0])
+        #        target = self.model.predict(state)
+        #        target[0, action] = reward
+        #        self.model.fit(state, target, epochs=1, verbose=False)
+
+        states      = np.vstack([b[0] for b in batch]).astype(np.float32)
+        actions     = np.array([b[1] for b in batch], dtype=np.int32)
+        next_states = np.vstack([b[2] for b in batch]).astype(np.float32)
+        rewards     = np.array([b[3] for b in batch], dtype=np.float32)
+        dones       = np.array([b[4] for b in batch], dtype=np.bool_)
+
+        # Q(s, :)
+        q_states = self.model(states, training=False).numpy()          # (B, A)
+
+        # max_a' Q(s', a')
+        q_next = self.model(next_states, training=False).numpy()       # (B, A)
+        max_q_next = np.max(q_next, axis=1)                            # (B,)
+
+        targets = q_states.copy()
+        targets[np.arange(self.batch_size), actions] = rewards + self.gamma * max_q_next * (~dones)
+
+         # One update only (FAST)
+        self.model.train_on_batch(states, targets)
+
+
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-            
+
     def learn(self, episodes):
         for e in range(1, episodes + 1):
             self.episodes += 1
