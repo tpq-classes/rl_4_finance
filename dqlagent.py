2e86979ac63b1cee2b92d581ef96d14e5cc46a39
tpq-classes <admin@tpq.io>
2026-08-19 12:51:47 +0200

    speedup for dqlagent

--- dqlagent.py
+++ dqlagent.py
@@ -12,8 +12,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from collections import deque
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
 
 warnings.simplefilter('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -29,8 +29,8 @@ class DQLAgent:
         self.epsilon = 1.0
         self.epsilon_decay = 0.9975
         self.epsilon_min = 0.1
-        self.memory = list()
-        self.batch_size = 128
+        self.memory = deque(maxlen=10000)
+        self.batch_size = 32
         self.gamma = 0.5
         self.trewards = deque(maxlen=2000)
         self.max_treward = -np.inf
@@ -38,7 +38,7 @@ class DQLAgent:
         self.env = env
         self.episodes = 0
         self._create_model(hu, lr)
-
+        
     def _create_model(self, hu, lr):
         self.model = Sequential()
         self.model.add(Dense(hu, activation='relu',
@@ -46,55 +46,42 @@ class DQLAgent:
         self.model.add(Dense(hu, activation='relu'))
         self.model.add(Dense(2, activation='linear'))
         self.model.compile(loss='mse', optimizer=opt(learning_rate=lr))
-
+        
     def _reshape(self, state):
         state = state.flatten()
         return np.reshape(state, [1, len(state)])
-
+            
     def act(self, state):
         if random.random() < self.epsilon:
             return self.env.action_space.sample()
-
-        #return np.argmax(self.model.predict(state)[0])
-
-        q = self.model(tf.convert_to_tensor(state, dtype=tf.float32), training=False)
-        return int(tf.argmax(q[0]).numpy())
-
+        # NEW SPEED OPTIMIZATION: call the model directly to avoid the overhead of model.predict() for a single row.
+        return np.argmax(self.model(state, training=False).numpy()[0])
+        # return np.argmax(self.model.predict(state)[0])
+        
     def replay(self):
         batch = random.sample(self.memory, self.batch_size)
-
-
-        #for state, action, next_state, reward, done in batch:
-        #    if not done:
-        #        reward += self.gamma * np.amax(
-        #            self.model.predict(next_state)[0])
-        #        target = self.model.predict(state)
-        #        target[0, action] = reward
-        #        self.model.fit(state, target, epochs=1, verbose=False)
-
-        states      = np.vstack([b[0] for b in batch]).astype(np.float32)
-        actions     = np.array([b[1] for b in batch], dtype=np.int32)
+        # NEW SPEED OPTIMIZATION: batch all predictions and train once instead of predicting/fitting once per sample.
+        states = np.vstack([b[0] for b in batch]).astype(np.float32)
+        actions = np.array([b[1] for b in batch], dtype=np.int64)
         next_states = np.vstack([b[2] for b in batch]).astype(np.float32)
-        rewards     = np.array([b[3] for b in batch], dtype=np.float32)
-        dones       = np.array([b[4] for b in batch], dtype=np.bool_)
-
-        # Q(s, :)
-        q_states = self.model(states, training=False).numpy()          # (B, A)
-
-        # max_a' Q(s', a')
-        q_next = self.model(next_states, training=False).numpy()       # (B, A)
-        max_q_next = np.max(q_next, axis=1)                            # (B,)
-
-        targets = q_states.copy()
-        targets[np.arange(self.batch_size), actions] = rewards + self.gamma * max_q_next * (~dones)
-
-         # One update only (FAST)
+        rewards = np.array([b[3] for b in batch], dtype=np.float32)
+        dones = np.array([b[4] for b in batch], dtype=np.bool_)
+        targets = self.model(states, training=False).numpy()
+        next_values = self.model(next_states, training=False).numpy()
+        targets[np.arange(len(batch)), actions] = (
+            rewards + self.gamma * np.max(next_values, axis=1) * (~dones)
+        )
         self.model.train_on_batch(states, targets)
-
-
+        # for state, action, next_state, reward, done in batch:
+        #     if not done:
+        #         reward += self.gamma * np.amax(
+        #             self.model.predict(next_state)[0])
+        #         target = self.model.predict(state)
+        #         target[0, action] = reward
+        #         self.model.fit(state, target, epochs=1, verbose=False)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-
+            
     def learn(self, episodes):
         for e in range(1, episodes + 1):
             self.episodes += 1
@@ -102,7 +89,6 @@ class DQLAgent:
             state = self._reshape(state)
             treward = 0
             for f in range(1, 5000):
-                self.f = f
                 action = self.act(state)
                 next_state, reward, done, trunc, _ = self.env.step(action)
                 treward += reward
@@ -117,7 +103,7 @@ class DQLAgent:
                     templ += f' | max={self.max_treward:7.2f}'
                     print(templ, end='\r')
                     break
-            if len(self.memory) > self.batch_size and (e % 5 == 0):
+            if len(self.memory) > self.batch_size:
                 self.replay()
         print()
         
@@ -133,8 +119,9 @@ class DQLAgent:
             state, _ = self.env.reset()
             state = self._reshape(state)
             for f in range(1, 5001):
-                q = self.model(tf.convert_to_tensor(state, dtype=tf.float32), training=False)
-                action = int(tf.argmax(q[0]).numpy())
+                # NEW SPEED OPTIMIZATION: call the model directly to avoid model.predict() overhead in the test loop.
+                action = np.argmax(self.model(state, training=False).numpy()[0])
+                # action = np.argmax(self.model.predict(state)[0])
                 state, reward, done, trunc, _ = self.env.step(action)
                 state = self._reshape(state)
                 if done:
